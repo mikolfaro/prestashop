a345e8a749a09c98007a349b9fe06b1ca3a930b9
Yiifi Fovan <yififof@mswork.ru>
2016-10-04 16:54:24 +0300

    Added mapping attributes to Customer & Address models

--- lib/prestashop/mapper/models/address.rb
+++ lib/prestashop/mapper/models/address.rb
@@ -4,6 +4,43 @@ module Prestashop
     class Address < Model
       resource :addresses
       model :address
+
+      attr_accessor :id_customer, :firstname, :lastname, :address1, :address2, :city, :phone, :phone_mobile, :id_country, :address_alias
+      attr_writer   :id
+
+      def initialize args = {}
+        @id            = args[:id]
+        @id_customer   = args.fetch(:id_customer)
+        @firstname     = args.fetch(:firstname)
+        @lastname      = args.fetch(:lastname)
+        @address1      = args.fetch(:address1)
+        @address2      = args[:address2]
+        @city          = args.fetch(:city)
+        @phone         = args[:phone]
+        @phone_mobile  = args.fetch(:phone_mobile, '')
+        @id_country    = args.fetch(:id_country)
+        @address_alias = args.fetch(:address_alias)
+      end
+
+      def id
+        @id ||= self.class.find_by 'filter[alias]' => address_alias
+      end
+      alias :find? :id
+
+      def hash
+        address = {
+          id_customer:  id_customer,
+          firstname:    firstname,
+          lastname:     lastname,
+          address1:     address1,
+          address2:     address2,
+          city:         city,
+          phone:        phone,
+          phone_mobile: phone_mobile,
+          id_country:   id_country,
+          alias:        address_alias
+        }
+      end
     end
   end
 end
--- lib/prestashop/mapper/models/customer.rb
+++ lib/prestashop/mapper/models/customer.rb
@@ -4,6 +4,37 @@ module Prestashop
     class Customer < Model
       resource :customers
       model :customer
+
+      attr_accessor       :firstname, :lastname, :email, :newsletter, :optin, :active, :passwd
+      attr_writer         :id
+
+      def initialize args = {}
+        @id         = args[:id]
+        @firstname  = args.fetch(:firstname)
+        @lastname   = args.fetch(:lastname)
+        @email      = args.fetch(:email)
+        @passwd     = args[:passwd]
+        @newsletter = args.fetch(:newsletter, 0)
+        @optin      = args.fetch(:optin, 1)
+        @active     = args.fetch(:active, 1)
+      end
+
+      def id
+        @id ||= self.class.find_by 'filter[email]' => email
+      end
+      alias :find? :id
+
+      def hash
+        customer = {
+          firstname:  firstname,
+          lastname:   lastname,
+          email:      email,
+          passwd:     passwd,
+          newsletter: newsletter,
+          optin:      optin,
+          active:     active
+        }
+      end
     end
   end
 end
