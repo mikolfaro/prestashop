bda37f4fcfdeed1080399213a4302cc69b3ceab9
Yiifi Fovan <yififof@mswork.ru>
2016-10-04 18:34:54 +0300

    Fixed  Address mapping; added State find_by_iso_code

--- lib/prestashop/mapper/models/address.rb
+++ lib/prestashop/mapper/models/address.rb
@@ -5,7 +5,7 @@ module Prestashop
       resource :addresses
       model :address
 
-      attr_accessor :id_customer, :firstname, :lastname, :address1, :address2, :city, :phone, :phone_mobile, :id_country, :address_alias
+      attr_accessor :id_customer, :firstname, :lastname, :address1, :address2, :city, :phone, :phone_mobile, :id_country, :id_state, :postcode, :address_alias
       attr_writer   :id
 
       def initialize args = {}
@@ -15,10 +15,12 @@ module Prestashop
         @lastname      = args.fetch(:lastname)
         @address1      = args.fetch(:address1)
         @address2      = args[:address2]
+        @id_state      = args[:id_state]
         @city          = args.fetch(:city)
         @phone         = args[:phone]
         @phone_mobile  = args.fetch(:phone_mobile, '')
         @id_country    = args.fetch(:id_country)
+        @postcode      = args[:postcode]
         @address_alias = args.fetch(:address_alias)
       end
 
@@ -34,10 +36,12 @@ module Prestashop
           lastname:     lastname,
           address1:     address1,
           address2:     address2,
+          id_state:     id_state,
           city:         city,
           phone:        phone,
           phone_mobile: phone_mobile,
           id_country:   id_country,
+          postcode:     postcode,
           alias:        address_alias
         }
       end
