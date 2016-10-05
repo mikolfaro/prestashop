064c01438ae6a12d23b7a9df76c51355ef5e47b5
ASergey <slide.step@gmail.com>
2016-10-05 11:44:26 +0300

    Added Customer default group

--- lib/prestashop/mapper/models/customer.rb
+++ lib/prestashop/mapper/models/customer.rb
@@ -5,18 +5,19 @@ module Prestashop
       resource :customers
       model :customer
 
-      attr_accessor       :firstname, :lastname, :email, :newsletter, :optin, :active, :passwd
+      attr_accessor       :firstname, :lastname, :email, :newsletter, :optin, :active, :passwd, :id_default_group
       attr_writer         :id
 
       def initialize args = {}
-        @id         = args[:id]
-        @firstname  = args.fetch(:firstname)
-        @lastname   = args.fetch(:lastname)
-        @email      = args.fetch(:email)
-        @passwd     = args[:passwd]
-        @newsletter = args.fetch(:newsletter, 0)
-        @optin      = args.fetch(:optin, 1)
-        @active     = args.fetch(:active, 1)
+        @id               = args[:id]
+        @firstname        = args.fetch(:firstname)
+        @lastname         = args.fetch(:lastname)
+        @email            = args.fetch(:email)
+        @passwd           = args[:passwd]
+        @id_default_group = args.fetch(:id_default_group, 3)
+        @newsletter       = args.fetch(:newsletter, 0)
+        @optin            = args.fetch(:optin, 0)
+        @active           = args.fetch(:active, 1)
       end
 
       def id
@@ -26,13 +27,14 @@ module Prestashop
 
       def hash
         customer = {
-          firstname:  firstname,
-          lastname:   lastname,
-          email:      email,
-          passwd:     passwd,
-          newsletter: newsletter,
-          optin:      optin,
-          active:     active
+          firstname:        firstname,
+          lastname:         lastname,
+          email:            email,
+          passwd:           passwd,
+          id_default_group: id_default_group,
+          newsletter:       newsletter,
+          optin:            optin,
+          active:           active
         }
       end
     end
